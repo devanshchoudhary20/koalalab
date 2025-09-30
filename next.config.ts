98c77cfaa56b7ee79313aefb94acbb16d8b40bf7
devanshchoudhary20 <devanshchoudhary999@gmail.com>
2025-09-30 17:18:43 +0000

    netlify fixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // Ensure compatibility with Netlify
+  experimental: {
+    esmExternals: false
+  }
 };
 
 export default nextConfig;
